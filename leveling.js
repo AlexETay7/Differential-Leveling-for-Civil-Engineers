0d9e402ef133709ce0da7d15bff5124cfa05b54f
Alex Taylor <alexetaylor7@gmail.com>
2023-12-24 12:20:46 -0700

    added javadocs

--- leveling.js
+++ leveling.js
@@ -1,15 +1,27 @@
-// leveling.js
-
+/**
+ * The back-end logic behind differential leveling.
+ * @author Alex Taylor 12/24/23
+ */
 let initialElevation;
 let currentHeightOfInstrument;
 let shotId = 1;
 
 function calculateShot() {
     // Get user inputs
-    initialElevation = parseFloat(document.getElementById('initialElevation').value);
+    let initialElevationInput = document.getElementById('initialElevation').value;
     let isBacksight = document.getElementById('isBacksight').checked;
     let description = document.getElementById('description').value;
-    let foresight = parseFloat(document.getElementById('foresight').value);
+    let foresightInput = document.getElementById('foresight').value;
+
+    // Parse inputs as floats
+    let initialElevation = parseFloat(initialElevationInput);
+    let foresight = parseFloat(foresightInput);
+
+    // Check if initialElevation and foresight are valid numbers
+    if (isNaN(initialElevation) || isNaN(foresight)) {
+        alert('Please enter valid numbers for Initial Elevation and Foresight.');
+        return;
+    }
 
     // Calculate elevation for the shot
     let elevation;
