3414b6073e1897e69337e5a23e20d0784bee3cac
Alex Taylor <alexetaylor7@gmail.com>
2023-12-25 23:06:44 -0700

    fixed variable declaration for best practices

--- leveling.js
+++ leveling.js
@@ -2,67 +2,80 @@
  * Differential Leveling back-end logic.
  * @author Alex Taylor 12/24/23
  */
-let initialElevation;
-let initialHeightOfInstrument;
-let currentHeightOfInstrument;
+
+// Cache frequently accessed DOM elements
+const initialElevationInput = document.getElementById('initialElevation');
+const descriptionInput = document.getElementById('description');
+const foresightInput = document.getElementById('foresight');
+const output = document.getElementById('output');
+
+// Survey state
+let initialElevation = null;
+let initialHeightOfInstrument = null;
+let currentHeightOfInstrument = null;
 let shotId = 1;
 let isBacksight = null;
 
+// Validation function for numeric inputs
+function isValidNumber(value, fieldName) {
+    if (isNaN(value)) {
+        alert(`Please enter a valid number for ${fieldName}.`);
+        return false;
+    }
+    return true;
+}
+
+// Set backsight value and update button styles
 function setBacksight(value) {
     isBacksight = value;
     updateButtonStyles();
 }
 
+// Update button styles based on backsight value
 function updateButtonStyles() {
     document.getElementById('yesButton').style.backgroundColor = isBacksight ? '#4caf50' : '#ccc';
     document.getElementById('noButton').style.backgroundColor = isBacksight === false ? '#4caf50' : '#ccc';
 }
 
+// Calculate shot based on user inputs
 function calculateShot() {
+    // Validate backsight selection
     if (isBacksight === null) {
         alert('Please select "Yes" or "No" for backsight.');
         return;
     }
 
     // Get user inputs
-    let initialElevationInput = document.getElementById('initialElevation').value.trim();
-    let description = document.getElementById('description').value.trim();
-    let foresightInput = document.getElementById('foresight').value.trim();
+    const initialElevationValue = parseFloat(initialElevationInput.value.trim());
+    const descriptionValue = descriptionInput.value.trim();
+    const foresightValue = parseFloat(foresightInput.value.trim());
 
-    // Parse inputs as floats
-    let initialElevation = parseFloat(initialElevationInput);
-    let foresight = parseFloat(foresightInput);
-
-    // Check if initialElevation and foresight are valid numbers
-    if (isNaN(initialElevation) || isNaN(foresight)) {
-        alert('Please enter valid numbers for Initial Elevation and Foresight.');
+    // Validate numeric inputs
+    if (!isValidNumber(initialElevationValue, 'Initial Elevation') || !isValidNumber(foresightValue, 'Foresight')) {
         return;
     }
 
     // Calculate initial height of the instrument
-    initialHeightOfInstrument = initialElevation + foresight;
+    initialHeightOfInstrument = initialElevationValue + foresightValue;
 
     // Calculate elevation for the shot
     let elevation;
     if (isBacksight) {
-        // For backsight, the initial height of the instrument is used
         currentHeightOfInstrument = initialHeightOfInstrument;
         elevation = currentHeightOfInstrument;
     } else {
-        // For foresight, the elevation is calculated based on the current height of the instrument
-        elevation = currentHeightOfInstrument - foresight;
+        elevation = currentHeightOfInstrument - foresightValue;
     }
 
     // Display the current state
-    let output = document.getElementById('output');
-    output.innerHTML += `<p>Shot ${shotId}: ${description}, Initial Elevation: ${initialElevation}, HoT: ${elevation}, HI: ${currentHeightOfInstrument}</p>`;
+    output.innerHTML += `<p>Shot ${shotId}: ${descriptionValue}, Initial Elevation: ${initialElevationValue}, HoT: ${elevation}, HI: ${currentHeightOfInstrument}</p>`;
 
     // Increment shotId
     shotId++;
 
     // Clear input fields
-    document.getElementById('description').value = "";
-    document.getElementById('foresight').value = "";
+    descriptionInput.value = '';
+    foresightInput.value = '';
     isBacksight = null;
     updateButtonStyles();
 }
