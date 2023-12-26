dc0558bfb65a33c3734a11ec31c4b1c329894a06
Alex Taylor <alexetaylor7@gmail.com>
2023-12-26 13:38:54 -0700

    fixed logic! works!

--- leveling.js
+++ leveling.js
@@ -1,86 +1,3 @@
-// /**
-//  * Differential Leveling back-end logic.
-//  * @author Alex Taylor 12/24/23
-//  */
-
-// // Cache frequently accessed DOM elements
-// const initialElevationInput = document.getElementById('initialElevation');
-// const descriptionInput = document.getElementById('description');
-// const foresightInput = document.getElementById('foresight');
-// const output = document.getElementById('output');
-
-// // Survey state
-// let initialElevation = null;
-// let initialHeightOfInstrument = null;
-// let currentHeightOfInstrument = null;
-// let shotId = 1;
-// let isBacksight = null;
-
-// // Validation function for numeric inputs
-// function isValidNumber(value, fieldName) {
-//     if (isNaN(value)) {
-//         alert(`Please enter a valid number for ${fieldName}.`);
-//         return false;
-//     }
-//     return true;
-// }
-
-// // Set backsight value and update button styles
-// function setBacksight(value) {
-//     isBacksight = value;
-//     updateButtonStyles();
-// }
-
-// // Update button styles based on backsight value
-// function updateButtonStyles() {
-//     document.getElementById('yesButton').style.backgroundColor = isBacksight ? '#4caf50' : '#ccc';
-//     document.getElementById('noButton').style.backgroundColor = isBacksight === false ? '#4caf50' : '#ccc';
-// }
-
-// // Calculate shot based on user inputs
-// function calculateShot() {
-//     // Validate backsight selection
-//     if (isBacksight === null) {
-//         alert('Please select "Yes" or "No" for backsight.');
-//         return;
-//     }
-
-//     // Get user inputs
-//     const initialElevationValue = parseFloat(initialElevationInput.value.trim());
-//     const descriptionValue = descriptionInput.value.trim();
-//     const foresightValue = parseFloat(foresightInput.value.trim());
-
-//     // Validate numeric inputs
-//     if (!isValidNumber(initialElevationValue, 'Initial Elevation') || !isValidNumber(foresightValue, 'Foresight')) {
-//         return;
-//     }
-
-//     // Calculate initial height of the instrument
-//     initialHeightOfInstrument = initialElevationValue + foresightValue;
-
-//     // Calculate height of target
-//     let HeightOfTarget;
-//     if (isBacksight) {
-//         currentHeightOfInstrument = initialHeightOfInstrument;
-//         //currentHeightOfInstrument = HeightOfTarget +
-//         //HeightOfTarget = currentHeightOfInstrument;
-//     } else {
-//         HeightOfTarget = currentHeightOfInstrument - foresightValue;
-//     }
-
-//     // Display the current state
-//     output.innerHTML += `<p>Shot ${shotId}: "${descriptionValue}" <strong>|</strong> Initial Elevation: ${initialElevationValue} <strong>|</strong> HT: ${HeightOfTarget} <strong>|</strong> HI: ${currentHeightOfInstrument} \n</p>`;
-
-//     // Increment shotId
-//     shotId++;
-
-//     // Clear input fields
-//     descriptionInput.value = '';
-//     foresightInput.value = '';
-//     isBacksight = null;
-//     updateButtonStyles();
-// }
-
 /**
  * Differential Leveling back-end logic.
  * @author Alex Taylor 12/24/23
@@ -96,6 +13,7 @@ const output = document.getElementById('output');
 let initialElevation = null;
 let initialHeightOfInstrument = null;
 let currentHeightOfInstrument = null;
+let previousHeightOfTarget = null; // Variable to store previous HT
 let shotId = 1; 
 let isBacksight = null;
 
@@ -146,12 +64,16 @@ function calculateShot() {
     if (isBacksight) {
         if (shotId === 1) {
             currentHeightOfInstrument = initialHeightOfInstrument;
+            // If it's the first backsight, set the previousHeightOfTarget to the initial HT
+            previousHeightOfTarget = currentHeightOfInstrument - shotValue;
         } else {
-            HeightOfTarget = currentHeightOfInstrument - shotValue;
+            HeightOfTarget = previousHeightOfTarget; // Use the previous HT for backsights
             currentHeightOfInstrument = HeightOfTarget + shotValue;
         }
     } else {
         HeightOfTarget = currentHeightOfInstrument - shotValue;
+        // Update the previousHeightOfTarget for future backsights
+        previousHeightOfTarget = HeightOfTarget;
     }
 
     // Display the current state
