4161d446a463ccbc9d30a4cc4e1e8f433ed0bbcb
Alex Taylor <alexetaylor7@gmail.com>
2023-12-26 13:22:52 -0700

    fixed some HoT issues, still needs work

--- leveling.js
+++ leveling.js
@@ -1,3 +1,86 @@
+// /**
+//  * Differential Leveling back-end logic.
+//  * @author Alex Taylor 12/24/23
+//  */
+
+// // Cache frequently accessed DOM elements
+// const initialElevationInput = document.getElementById('initialElevation');
+// const descriptionInput = document.getElementById('description');
+// const foresightInput = document.getElementById('foresight');
+// const output = document.getElementById('output');
+
+// // Survey state
+// let initialElevation = null;
+// let initialHeightOfInstrument = null;
+// let currentHeightOfInstrument = null;
+// let shotId = 1;
+// let isBacksight = null;
+
+// // Validation function for numeric inputs
+// function isValidNumber(value, fieldName) {
+//     if (isNaN(value)) {
+//         alert(`Please enter a valid number for ${fieldName}.`);
+//         return false;
+//     }
+//     return true;
+// }
+
+// // Set backsight value and update button styles
+// function setBacksight(value) {
+//     isBacksight = value;
+//     updateButtonStyles();
+// }
+
+// // Update button styles based on backsight value
+// function updateButtonStyles() {
+//     document.getElementById('yesButton').style.backgroundColor = isBacksight ? '#4caf50' : '#ccc';
+//     document.getElementById('noButton').style.backgroundColor = isBacksight === false ? '#4caf50' : '#ccc';
+// }
+
+// // Calculate shot based on user inputs
+// function calculateShot() {
+//     // Validate backsight selection
+//     if (isBacksight === null) {
+//         alert('Please select "Yes" or "No" for backsight.');
+//         return;
+//     }
+
+//     // Get user inputs
+//     const initialElevationValue = parseFloat(initialElevationInput.value.trim());
+//     const descriptionValue = descriptionInput.value.trim();
+//     const foresightValue = parseFloat(foresightInput.value.trim());
+
+//     // Validate numeric inputs
+//     if (!isValidNumber(initialElevationValue, 'Initial Elevation') || !isValidNumber(foresightValue, 'Foresight')) {
+//         return;
+//     }
+
+//     // Calculate initial height of the instrument
+//     initialHeightOfInstrument = initialElevationValue + foresightValue;
+
+//     // Calculate height of target
+//     let HeightOfTarget;
+//     if (isBacksight) {
+//         currentHeightOfInstrument = initialHeightOfInstrument;
+//         //currentHeightOfInstrument = HeightOfTarget +
+//         //HeightOfTarget = currentHeightOfInstrument;
+//     } else {
+//         HeightOfTarget = currentHeightOfInstrument - foresightValue;
+//     }
+
+//     // Display the current state
+//     output.innerHTML += `<p>Shot ${shotId}: "${descriptionValue}" <strong>|</strong> Initial Elevation: ${initialElevationValue} <strong>|</strong> HT: ${HeightOfTarget} <strong>|</strong> HI: ${currentHeightOfInstrument} \n</p>`;
+
+//     // Increment shotId
+//     shotId++;
+
+//     // Clear input fields
+//     descriptionInput.value = '';
+//     foresightInput.value = '';
+//     isBacksight = null;
+//     updateButtonStyles();
+// }
+
 /**
  * Differential Leveling back-end logic.
  * @author Alex Taylor 12/24/23
@@ -6,14 +89,14 @@
 // Cache frequently accessed DOM elements
 const initialElevationInput = document.getElementById('initialElevation');
 const descriptionInput = document.getElementById('description');
-const foresightInput = document.getElementById('foresight');
+const shotInput = document.getElementById('foresight'); 
 const output = document.getElementById('output');
 
 // Survey state
 let initialElevation = null;
 let initialHeightOfInstrument = null;
 let currentHeightOfInstrument = null;
-let shotId = 1;
+let shotId = 1; 
 let isBacksight = null;
 
 // Validation function for numeric inputs
@@ -48,23 +131,27 @@ function calculateShot() {
     // Get user inputs
     const initialElevationValue = parseFloat(initialElevationInput.value.trim());
     const descriptionValue = descriptionInput.value.trim();
-    const foresightValue = parseFloat(foresightInput.value.trim());
+    const shotValue = parseFloat(shotInput.value.trim()); 
 
     // Validate numeric inputs
-    if (!isValidNumber(initialElevationValue, 'Initial Elevation') || !isValidNumber(foresightValue, 'Foresight')) {
+    if (!isValidNumber(initialElevationValue, 'Initial Elevation') || !isValidNumber(shotValue, 'Foresight')) {
         return;
     }
 
     // Calculate initial height of the instrument
-    initialHeightOfInstrument = initialElevationValue + foresightValue;
+    initialHeightOfInstrument = initialElevationValue + shotValue; 
 
     // Calculate height of target
     let HeightOfTarget;
     if (isBacksight) {
-        currentHeightOfInstrument = initialHeightOfInstrument;
-        //HeightOfTarget = currentHeightOfInstrument;
+        if (shotId === 1) {
+            currentHeightOfInstrument = initialHeightOfInstrument;
+        } else {
+            HeightOfTarget = currentHeightOfInstrument - shotValue;
+            currentHeightOfInstrument = HeightOfTarget + shotValue;
+        }
     } else {
-        HeightOfTarget = currentHeightOfInstrument - foresightValue;
+        HeightOfTarget = currentHeightOfInstrument - shotValue;
     }
 
     // Display the current state
@@ -75,7 +162,7 @@ function calculateShot() {
 
     // Clear input fields
     descriptionInput.value = '';
-    foresightInput.value = '';
+    shotInput.value = ''; 
     isBacksight = null;
     updateButtonStyles();
 }
